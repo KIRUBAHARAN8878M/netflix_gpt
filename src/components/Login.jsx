7dc76d0efb2d791afe7bb4712fda905cdc49df06
Kirubaharan <kirubaharan@phantomfx.com>
2025-07-03 15:36:27 +0530

    updated code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
+const email = useRef();
+const password = useRef();
+
+  const handleButtonClick = () => {
+      const message = checkValidData(email.current.value, password.current.value)
+      setErrorMessage(message);
+  }
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -16,7 +27,7 @@ const Login = () => {
           alt="bg_img"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-85">
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-85" onSubmit={(e)=>e.preventDefault()}>
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -28,16 +39,19 @@ const Login = () => {
           />
         )}
         <input
+        ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <input
+        ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+        <p className="font-bold text-red-600">{errorMessage}</p>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
